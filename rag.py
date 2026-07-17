9b54c5c8955ca35bab1c55e52d41586f39be2a50
Bilal anas ka <bilal.khalid.bsai-2024a@cecosian.edu.pk>
2026-07-17 23:13:44 +0500

    added structred outputs

--- rag.py
+++ rag.py
@@ -7,11 +7,39 @@ from llama_index.core import VectorStoreIndex
 from dotenv import load_dotenv
 import os
 from llama_index.core.chat_engine.types import ChatMode
-
+from typing import Literal
+from pydantic import BaseModel, Field
 # Import the env keys
 load_dotenv(dotenv_path=r'F:\udemy\mera-llama-index-project\.env')
-# LLM Config
-llm = Groq(model='qwen/qwen3-32b', api_key=os.getenv('GROQ_API_KEY_FREE_1'))
+
+# Raw LLM Config
+raw_llm = Groq(model='qwen/qwen3-32b', api_key=os.getenv('GROQ_API_KEY_FREE_1'), strict = True)
+
+# Pydantic Validation
+class Structred_LLM_Output(BaseModel):
+    input_category : Literal['Greeting', 'Query', 'Greeting and Query', 'something else'] = Field(
+        description=""" 1. <Greeting> if input == Greeting/chit chat.</Greeting> 
+                        2. <Query> if input == User Query </Query> 
+                        3. <Greeting and Query> if input == Greeting as well as a User Query.</Greeting and Query>
+                        4. <something else> if the specific class for user's input is not the previous three</something else>
+                    """
+    )
+    escalate_to_human: bool | None | Literal['bad_input']= Field(
+        description="""1. <null> if Field **input_category** == Greeting </null> 
+                        2.<true> if the answer to the user's query is not availble in the **Provided Context Documents** or **Conversation History**</true> 
+                        3.<false> if the answer to the user's query is avaible in the **Provided Context Documents** or **Conversation History**</false>
+                        4.<bad_input> if Field **input_category** == something else</bad_input>
+        """
+    )
+    Response : str | None= Field(
+        description= """
+            <null> if Field **escalate_to_human** == true</null>
+            <str> if Field **escalate_to_human** == false. Entertain/Satisfy the user's query<str>
+        """
+    )
+
+# RAG LLM
+llm = raw_llm.as_structured_llm(output_cls=Structred_LLM_Output)
 
 # EMBED MODEL Config
 embed_model = CohereEmbedding(cohere_api_key=os.getenv('cohere_api_key_1'), model_name="embed-english-v3.0")
@@ -31,52 +59,16 @@ index = VectorStoreIndex.from_vector_store(vector_store=pc_vector_store, embed_m
 system_prompt = """
 <system_instructions>
 <persona>
-You are a direct, straightforward Customer Support Representative assisting users with "Product Return Queries" based on "Product Return Policies".  You must be professional, helpful, and keep the conversation going to resolve the user's issue. 
+You are Customer Support Representative assisting users with "Product Return Queries" based on "Product Return Policies".  You must be professional, helpful, and keep the conversation going to resolve the user's issue/query. 
 </persona>
-
-<rules>
-1. CLASSIFY INPUT: First, decide if the user's message is a greeting (e.g., "Hi", "Hello") or casual chit-chat.
-   - If it IS a greeting, acknowledge it politely, skip document checking, and ask how you can help with their product return. Do NOT output "#".
-   
-2. RAG RETRIEVAL: If the user is asking a specific question about product returns:
-   - You must strictly use ONLY the **provided context documents** as well as the **CONVERSATION HISTORY**.
-   - If the answer to their return query is NOT explicitly found in the **Provided Documents** and **Conversation History**, you must output ONLY this single character and absolutely nothing else: #
-</rules>
-
 <format>
 - Tone: Direct and straightforward (No BS).
 - Style: Bold the most important words or instructions for easy readability.
-- Note: The bolding rule does not apply if you are outputting the fallback "#" character.
-- Length: Keep responses under 100 words.
+- Length: Keep responses under 150 words.
 </format>
 </system_instructions>
 """
 # Chat Engine
 my_chat_engine = index.as_chat_engine(llm=llm,  system_prompt = system_prompt,chat_mode=ChatMode.CONDENSE_PLUS_CONTEXT, similarity_top_k = 6, node_postprocessors = [reranker])
 
-
-if __name__ == "__main__":
-
-    while True:
-        user_query = input('Please enter your query: ')
-        if user_query:
-            cancel_chat = ['stop', 'exit', 'break']
-            if user_query.lower().strip() in cancel_chat:
-                print('\n')
-                print('-' * 20, ' breaking the chat ', '-' * 20)
-                break
-            raw_response = my_chat_engine.chat(user_query)
-            ai_response = raw_response.response.split('</think>')[-1].strip()
-            print('AI Respons: ', ai_response)
-            print('-'*20, f' Source Docs  ', '-' * 20)
-            for node in raw_response.source_nodes :
-                print('-' * 50)
-                print(node.get_content())
-            
-            print('='*50)
-            print('\n')
-
-    ############################################################################################
-    # STEP 1: Define the Python Function for routing the Query to Customer Support via Gmail
-    ############################################################################################
 
