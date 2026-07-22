7aa4295729b5cd9944f2a78428b86fd44b09e56e
Bilal anas ka <bilal.khalid.bsai-2024a@cecosian.edu.pk>
2026-07-22 17:08:47 +0500

    added logging system

--- chatbot.py
+++ chatbot.py
@@ -2,26 +2,41 @@ import gradio as gr
 from rag import my_chat_engine
 from llama_index.core.llms import ChatMessage, MessageRole
 import os
+from rag import logger
 def my_chatbot(message, history):
     chat_history = []
-    for msg in history:
-        if msg['role'] == 'user':
-            user_msg = msg['content'][0]['text']
-            chat_history.append(ChatMessage(role =MessageRole.USER , content= user_msg)) 
+    response = "Nothing yet"
+    try:
+        try:
+            for msg in history:
+                if msg['role'] == 'user':
+                    user_msg = msg['content'][0]['text']
+                    chat_history.append(ChatMessage(role =MessageRole.USER , content= user_msg)) 
+                    logger.info(chat_history)
 
-        elif msg['role'] == 'assistant':
-            ai_msg = msg['content'][0]['text']
-            chat_history.append(ChatMessage(role = MessageRole.ASSISTANT, content = ai_msg))
-        else:
-            raise ValueError('there is an error in the chat history loop')
+                elif msg['role'] == 'assistant':
+                    ai_msg = msg['content'][0]['text']
+                    chat_history.append(ChatMessage(role = MessageRole.ASSISTANT, content = ai_msg))
+                    logger.info(chat_history)
 
-    response = my_chat_engine.chat(message=message, chat_history=chat_history).response #.split('</think>')[-1].strip()
+        except Exception as e:        
+            logger.critical(chat_history)
+            logger.exception('there is an error in chat hsitory')
+
+        response = my_chat_engine.chat(message=message, chat_history=chat_history).response #.split('</think>')[-1].strip()
+        logger.info(response)
+        
+        return response            
+                
+    except Exception as e:
+        logger.critical(response)
+        logger.exception("error in teh 'My_chatbot' function ")
 
     
-    return response
 
 
 demo = gr.ChatInterface(fn=my_chatbot, save_history=True, textbox=gr.Textbox(placeholder='enter your query hurry'))
 
 demo.launch(server_name="0.0.0.0", server_port=int(os.environ.get("PORT", 7860)))
+
     
--- rag.py
+++ rag.py
@@ -9,31 +9,41 @@ import os
 from llama_index.core.chat_engine.types import ChatMode
 from typing import Literal
 from pydantic import BaseModel, Field
+import logging
+import sys
 # Import the env keys
 load_dotenv()
 
-
+logging.basicConfig(
+    level=logging.INFO,
+    format='%(asctime)s - %(levelname)s - %(message)s',
+    handlers=[logging.StreamHandler(sys.stdout)]
+)
+logger = logging.getLogger(__name__)
 
 
     
 # 4. <something_else> If the input is something that cannot be categorized in teh previous 3 categories </something_else>               - false: if  **input_category** is **something_else**.
 # RAG LLM
 llm = Groq(model='qwen/qwen3.6-27b', api_key=os.getenv('GROQ_API_KEY_FREE_1'),temperature = 0.0, additional_kwargs={"extra_body": {"reasoning_format": "parsed"}})
-
+logger.info('GROQ LLM INTIALIZED')
 # EMBED MODEL Config
-embed_model = CohereEmbedding(cohere_api_key=os.getenv('cohere_api_key_1'), model_name="embed-english-v3.0")
 
+embed_model = CohereEmbedding(cohere_api_key=os.getenv('cohere_api_key_1'), model_name="embed-english-v3.0")
+logger.info('embed model created')
 # Reranker
+
 reranker = CohereRerank(api_key=os.getenv("cohere_api_key_1"), model='rerank-v4.0-fast', top_n=4) 
+logger.info('reranker  created')
 # Pinecone
 INDEX_NAME = 'neva-tech-cohere-policy-1'
 pc = Pinecone(api_key=os.getenv('Second-api-key'))
 pc_index = pc.Index(INDEX_NAME)
 pc_vector_store = PineconeVectorStore(pinecone_index=pc_index)
-
+logger.info('PIneocne db intialized')
 # Vector Index
 index = VectorStoreIndex.from_vector_store(vector_store=pc_vector_store, embed_model=embed_model)
-
+logger.info('VEctor store index intialized')
 # SYSTEM PROMPT
 system_prompt = """
 <system_instructions>
@@ -76,3 +86,4 @@ Length: Keep responses under 150 words.
 """
 # Chat Engine
 my_chat_engine = index.as_chat_engine(llm=llm,  system_prompt = (system_prompt),chat_mode=ChatMode.CONDENSE_PLUS_CONTEXT, similarity_top_k = 6, node_postprocessors = [reranker])
+logger.info('Chat Engine intialized')
